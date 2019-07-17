b58e2f51afb0ee36d9a6705644579ed0ab38eb4c
liu330786193 <liu19910406>
2019-07-17 16:04:24 +0800

    -- 第一次提交

--- src/main/java/com/lyl/commit/CommitApplication.java
+++ src/main/java/com/lyl/commit/CommitApplication.java
@@ -8,6 +8,11 @@ public class CommitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CommitApplication.class, args);
+
+        System.out.println("初始1");
+
     }
+
+
 
 }
