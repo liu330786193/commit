b8ef32611ded65656c6715c2d7532efea34298f2
liu330786193 <liu19910406>
2019-07-17 15:41:42 +0800

    first commit

---
+++ src/main/java/com/lyl/commit/CommitApplication.java
@@ -0,0 +1,13 @@
+package com.lyl.commit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommitApplication.class, args);
+    }
+
+}
